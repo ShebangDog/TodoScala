63cd128b7f0af1347daf30be1e27e993d9942166
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-08-27 21:14:25 +0900

    add test for parseAsTodo

--- src/main/scala/usecase/todoservice/impl/TodoServiceImpl.scala
+++ src/main/scala/usecase/todoservice/impl/TodoServiceImpl.scala
@@ -35,3 +35,50 @@ object TodoService extends TodoService {
     liftedExceptionEither.flatMap(creator.create.andThen(_.leftMap(liftToTodoRepositoryError)))
   }
 }
+
+class TodoServiceTest extends AnyFunSpec {
+  describe("TodoService") {
+    case class MockState(rawUuid: String, nowTime: Long)
+    given UUIDGen[CurriedState[MockState]] with {
+      override def randomUUID: CurriedState[MockState][UUID] = State[MockState, UUID] { mockState =>
+        val MockState(uuid, _) = mockState
+
+        (mockState, UUID.fromString(uuid))
+      }
+    }
+
+    given Clock[CurriedState[MockState]] with {
+      override def realTime: CurriedState[MockState][FiniteDuration] = State[MockState, FiniteDuration] { mockState =>
+        val MockState(_, nowTime) = mockState
+
+        (mockState, FiniteDuration(nowTime, TimeUnit.MILLISECONDS))
+      }
+    }
+
+    describe("parseAsTodo") {
+      case class Input(rawUuid: String, rawTitle: String, rawDescription: String, nowTime: Long)
+      case class TestCase(input: Input, expected: Right[Throwable, Todo])
+      val testCaseList: List[TestCase] = List(
+        TestCase(
+          Input("a5f9c478-01c0-4c0d-abcd-ee189b28fca1", "rawTitle", "rawDescription", 1627980601000L),
+          Right(
+            Todo(UUID.fromString("a5f9c478-01c0-4c0d-abcd-ee189b28fca1"), "rawTitle", "rawDescription", 1627980601000L, 1627980601000L)
+          )
+        )
+      )
+
+      testCaseList.foreach { case TestCase(input, expected) =>
+        describe(s"when call with $input") {
+          val Input(rawUuid, rawTitle, rawDescription, nowTime) = input
+          val (_, result) = parseAsTodo[CurriedState[MockState]](rawTitle, rawDescription).value
+            .run(MockState(rawUuid, nowTime))
+            .value
+
+          it(s"should return $expected") {
+            assert(expected == result)
+          }
+        }
+      }
+    }
+  }
+}
