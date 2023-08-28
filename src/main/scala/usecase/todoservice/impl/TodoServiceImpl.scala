bdfaedca7455875d0c227a029a9266ede7af64ae
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-08-28 18:14:13 +0900

    add left case

--- src/main/scala/usecase/todoservice/impl/TodoServiceImpl.scala
+++ src/main/scala/usecase/todoservice/impl/TodoServiceImpl.scala
@@ -1,19 +1,18 @@
 package dog.shebang
 package usecase.todoservice.impl
 
-import domain.todo.{Todo, TodoUtil}
+import domain.todo.Todo
 import repository.*
 import repository.creator.*
-import repository.reader.{ReadError, Reader}
-import usecase.todoservice.parse.todo.parseAsTodo
+import usecase.todoservice.parse.todo.{ParseDescriptionError, ParseError, ParseTitleError, parseAsTodo}
 import usecase.todoservice.{TodoRepositoryError, TodoService, TodoServiceError}
 import utility.typeclass.clock.Clock
 import utility.typeconstructor.CurriedState
 
 import cats.Monad
 import cats.data.{EitherT, State}
+import cats.effect.std
 import cats.effect.std.UUIDGen
-import cats.effect.{IO, std, Clock as CatsClock}
 import cats.syntax.functor.*
 import io.github.iltotore.iron.autoRefine
 import org.scalatest.funspec.AnyFunSpec
@@ -21,7 +20,6 @@ import org.scalatest.funspec.AnyFunSpec
 import java.util.UUID
 import java.util.concurrent.TimeUnit
 import scala.collection.mutable
-import scala.collection.mutable.Stack
 import scala.concurrent.duration.FiniteDuration
 
 object TodoService extends TodoService {
@@ -56,32 +54,38 @@ class TodoServiceTest extends AnyFunSpec {
     }
 
     describe("parseAsTodo") {
-      describe("success") {
-        case class Input(rawUuid: String, rawTitle: String, rawDescription: String, nowTime: Long)
-        case class TestCase(input: Input, expected: Right[Throwable, Todo])
-        val testCaseList: List[TestCase] = List(
-          TestCase(
-            Input("a5f9c478-01c0-4c0d-abcd-ee189b28fca1", "rawTitle", "rawDescription", 1627980601000L),
-            Right(
-              Todo(UUID.fromString("a5f9c478-01c0-4c0d-abcd-ee189b28fca1"), "rawTitle", "rawDescription", 1627980601000L, 1627980601000L)
-            )
+      case class TestCase[T[_, _]](input: Input, expected: T[ParseError, Todo])
+      case class Input(rawUuid: String, rawTitle: String, rawDescription: String, nowTime: Long)
+
+      val failureTestCaseList: List[TestCase[Left]] = List(
+        TestCase(Input("a5f9c478-01c0-4c0d-abcd-ee189b28fca1", "", "rawDescription", 1627980601000L), Left[ParseError, Todo](ParseTitleError)),
+        TestCase(Input("a5f9c478-01c0-4c0d-abcd-ee189b28fca1", "rawTitle", "", 1627980601000L), Left[ParseError, Todo](ParseDescriptionError)),
+      )
+
+      val successTestCaseList: List[TestCase[Right]] = List(
+        TestCase(
+          Input("a5f9c478-01c0-4c0d-abcd-ee189b28fca1", "rawTitle", "rawDescription", 1627980601000L),
+          Right(
+            Todo(UUID.fromString("a5f9c478-01c0-4c0d-abcd-ee189b28fca1"), "rawTitle", "rawDescription", 1627980601000L, 1627980601000L)
           )
         )
+      )
 
-        testCaseList.foreach { case TestCase(input, expected) =>
-          describe(s"when call with $input") {
-            val Input(rawUuid, rawTitle, rawDescription, nowTime) = input
-            val (_, result) = parseAsTodo[CurriedState[MockState]](rawTitle, rawDescription).value
-              .run(MockState(rawUuid, nowTime))
-              .value
+      val testCaseList: List[TestCase[Right] | TestCase[Left]] = successTestCaseList ++ failureTestCaseList
 
-            it(s"should return $expected") {
-              assert(expected == result)
-            }
+      testCaseList.foreach { case TestCase(input, expected) =>
+        describe(s"when call with $input") {
+          val Input(rawUuid, rawTitle, rawDescription, nowTime) = input
+          val (_, result) = parseAsTodo[CurriedState[MockState]](rawTitle, rawDescription).value
+            .run(MockState(rawUuid, nowTime))
+            .value
+
+          it(s"should return $expected") {
+            assert(expected == result)
           }
         }
-
       }
     }
   }
+
 }
