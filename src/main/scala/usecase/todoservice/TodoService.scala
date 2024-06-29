fa5909303429472fd73663df0f92417d5b435c3d
ShebangDog <twilight.sky.asterisk@gmail.com>
2024-06-29 19:42:04 +0900

    feat: add read as service of todo

--- src/main/scala/usecase/todoservice/TodoService.scala
+++ src/main/scala/usecase/todoservice/TodoService.scala
@@ -8,7 +8,12 @@ import utility.typeclass.clock.Clock
 import cats.Monad
 import cats.data.EitherT
 import cats.effect.std.UUIDGen
+import java.util.UUID
+import dog.shebang.domain.todo.Todo
+import dog.shebang.repository.reader.Reader
 
 trait TodoService {
-  def save[F[_] : Monad](using UUIDGen[F], Creator[F], Clock[F])(rawTitle: String, rawDescription: String): EitherT[F, TodoServiceError, Unit]
+  def save[F[_] : Monad](using UUIDGen[F], Creator[F], Clock[F])(rawTitle: String, rawDescription: String): EitherT[F, TodoServiceError, UUID]
+
+  def read[F[_] : Monad](using Reader[F])(id: UUID): EitherT[F, TodoServiceError, Todo]
 }
